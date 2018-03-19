25d6bade24419f65bca107efd1e4531e254936b6
warrior <warrior@WARRIORAVI-PC>
2018-03-20 04:30:08 +0530

    First Project Commit

---
+++ ObjectKeyWordDriven/src/main/java/ReadObject.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+/**
+ * 
+ */
+
+/**
+ * @author AvinashSrivastava
+ *
+ */
+public class ReadObject 
+{
+	 Properties p = new Properties();
+	    public Properties getObjectRepository() throws IOException{
+	        //Read object repository file
+	        InputStream stream = new FileInputStream(new File(System.getProperty("user.dir")+"\\object.properties"));
+	        //load all objects
+	        p.load(stream);
+	         return p;
+	    }
+}
